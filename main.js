f63b85099a25737f3ea2b529e7c88112ed416f03
stankoj <stanko.jankovic@live.com>
2023-10-08 01:04:09 +0200

    just another commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//classes
 class World {
     constructor() {
         this.height = 30;
@@ -11,35 +12,86 @@ class World {
 
     update() {
         for (var i = 0; i < this.squares.length; i++) {
-            if (this.squares[i].isConnected) {
+            let outputs=this.squares[i].calculateOutputs();
+            if (outputs["right"]) {
+                this.squares[i].x+=SQUARESIZE;
+            }
+            if (outputs["left"]) {
+                this.squares[i].x-=SQUARESIZE;
+            }
+            if (outputs["up"]) {
+                this.squares[i].y+=SQUARESIZE;
+            }
+            if (outputs["down"]) {
+                this.squares[i].y-=SQUARESIZE;
+            }
+        }
+    };
 
-            }
-            else {
-                document.body.appendChild(this.squares[i].graphic);
-            }
+    draw() {
+        const canvas = document.getElementById("flatland");
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < this.squares.length; i++) {
+            ctx.beginPath();
+            ctx.rect(this.squares[i].x, this.squares[i].y, this.squares[i].width, this.squares[i].height);
+            ctx.fillStyle = this.squares[i].color;
+            ctx.fill();
+            ctx.closePath();
         }
     };
   }
 
 class Being {
     constructor() {
-        this.graphic = document.createElement('div');
-        this.graphic.style.width=squareSize+"px";
-        this.graphic.style.height=squareSize+"px";
-        this.graphic.style.backgroundColor="red";
+        this.width=SQUARESIZE;
+        this.height=SQUARESIZE;
+        this.x=50;
+        this.y=50;
+        this.color="red";
     }
     getInputs() {};
-    calculateOutputs() {};
+    calculateOutputs() {
+        var outputs = {
+            "up": null,
+            "down": null,
+            "left":null,
+            "right":null
+          };
+        for(var key in outputs) {
+            outputs[key] = Math.random() < 0.5;
+          }
+        return outputs;
+    };
 }
 
 //main
+const SQUARESIZE = 10;
+var slowdown=1000;
+var frameRate=1000/60;
 
-const squareSize = 10;
-
-world = new World();
+//create world
+var world = new World();
 world.addSquare(new Being());
 
-while (true) {
-    world.update();
-    break;
+
+//game loop
+var lastUpdate=0;
+var lastDraw=0;
+function gameLoop(timeStamp) {
+    //slow down world update based on "slowdown" parameter
+    if (timeStamp-lastUpdate>slowdown) {
+        world.update();
+        lastUpdate=timeStamp;
+    }
+    //draw only if frame rate time has passed
+    if (timeStamp-lastDraw>frameRate) {
+        world.draw();
+        lastDraw=timeStamp;
+    }
+    requestAnimationFrame(gameLoop);
 }
+
+
+//run game
+gameLoop();
