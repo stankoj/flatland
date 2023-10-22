d9e072e7d2a5fb39c7c05b665127ef67741fc7e1
stankoj <stanko.jankovic@live.com>
2023-10-23 00:42:00 +0200

    cleaned up code

--- main.js
+++ main.js
@@ -1,47 +1,60 @@
-//classes
+/////////////////////////////////////////////////////////////////////
+//    ______ _            _______ _               _   _ _____      //
+//   |  ____| |        /\|__   __| |        /\   | \ | |  __ \     //
+//   | |__  | |       /  \  | |  | |       /  \  |  \| | |  | |    //
+//   |  __| | |      / /\ \ | |  | |      / /\ \ | . ` | |  | |    //
+//   | |    | |____ / ____ \| |  | |____ / ____ \| |\  | |__| |    //
+//   |_|    |______/_/    \_\_|  |______/_/    \_\_| \_|_____/     //
+//                                                                 //
+//   stanko.jankovic@live.com                                      //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+
+
+// Globsl config
+
+var config = {
+"worldHeight" : 30,
+"worldWidth" : 30,
+"grassPercentange" : 0.1,
+"speed" : 1000/2,
+"squareSize" : 10
+}
+
+
+// Classes
+
+// Thw World class represents the world an its content
 class World {
     constructor() {
-        this.height = 30;
-        this.width = 30;
-        this.squares = [];
+        // World properties
+        this.height = config.worldHeight;
+        this.width = config.worldWidth;
         this.state=Array.from(new Array(this.height), () => new Array(this.width).fill())
-        //stats
+
+        // Stats
         this.life = 0;
         this.age = 0;
         this.maxAge = 0;
-        this.grassPercentage = 0.1;
+        this.grassPercentage = config.grassPercentange;
     }
 
-    addSquare(square) {
-        this.squares.push(square);
-    };
-
-    addActor(state) {
-        while (true) {
-            var i = Math.floor(Math.random() * this.height);
-            var j = Math.floor(Math.random() * this.width);
-            if (state[i][j].length > 1) {
-                continue;
-            }
-            state[i][j].push(new Robot());
-            break;
+    // Function to add a new element to the world
+    addSquare(state, Class, x = "random", y = "random") {
+        if (x == "random" || y == "random") {
+            x = Math.floor(Math.random() * this.width);
+            y = Math.floor(Math.random() * this.height);
         }
-    }
-
-    addGrass(state) {
-        while (true) {
-            var i = Math.floor(Math.random() * this.height);
-            var j = Math.floor(Math.random() * this.width);
-            if (this.state[i][j].length > 1) {
-                continue;
-            }
-            state[i][j].push(new Grass());
-            break;
+        if (state[x][y].length > 1) {
+            return false;
         }
+        state[x][y].push(new Class());
+        return true;
     }
 
+    // Function to generate the world
     generate() {
-        //generate terrain
+        // Generate terrain
         for (let i = 0; i < this.height; i++) {
             for (let j = 0; j < this.width; j++) {
                 this.state[i][j] = new Array();
@@ -49,7 +62,7 @@ class World {
             };
         };
 
-        //generate border
+        // Generate border
         for (var i = 0; i < this.height; i++) {
             for (var j = 0; j < this.width; j++) {
                 if (i == 0 || i == this.height-1 || j == 0 || j == this.width-1) {
@@ -58,23 +71,20 @@ class World {
             }
         }
 
-        //generate solids
-
-        //generate grass
+        // Generate grass
         var grassNumber = this.height * this.width * this.grassPercentage;
         while (grassNumber > 0) {
-            this.addGrass(this.state);
+            while (!this.addSquare(this.state, Grass));
             grassNumber--;
         }
 
-        //generate actors
-        this.addActor(this.state);
+        // Generate actors
+        while (!this.addSquare(this.state, Robot));
     }
 
-
+    // Function to update the world state for one timestep
     update() {
-
-        // collect objects that need to be updated and calculate next action on them
+        // Collect objects that need to be updated and calculate next action on them
         var actions = [];
         for (let i = 0; i < this.height; i++) {
             for (let j = 0; j < this.width; j++) {
@@ -95,34 +105,34 @@ class World {
                         if (outputs["down"]) {
                             y+=1;
                         }
-                        actions.push({"object":this.state[i][j][k],"currentLocation":[i,j],"nextLocation":[x,y],"conflict":false});
+                        actions.push({"object":this.state[i][j][k],"currentLocation":[i,j],"newLocation":[x,y],"conflict":false});
                     }
             };
         };
 
-        // resolve conflict on all objects and flag invalid actions
+        // Resolve conflict on all objects and flag invalid actions
         var runConflictLoop = true;
         while (runConflictLoop) {
             runConflictLoop = false;
             for (let i = 0; i < actions.length; i++) {
-                // do not check if action already marked as conflicting in previous loop iterations 
+                // Do not check if action already marked as conflicting in previous loop iterations 
                 if (actions[i]["conflict"]==true) {
                     continue;
                 }
-                // check if any other object is planning the move to same field
+                // Check if any other object is planning the move to same field
                 for (let j = 0; j < actions.length; j++) {
                     if (i == j) {
                         continue;
                     }
-                    if (JSON.stringify(actions[i]["nextLocation"]) == JSON.stringify(actions[j]["nextLocation"])) {
+                    if (JSON.stringify(actions[i]["newLocation"]) == JSON.stringify(actions[j]["newLocation"])) {
                         actions[i]["conflict"]=true;
                         actions[j]["conflict"]=true;
                         runConflictLoop = true;
                         break;
                     }
                 }
-                // check what is already present on that field
-                var newField = this.state[actions[i]["nextLocation"][0]][actions[i]["nextLocation"][1]];
+                // Check what is already present on that field
+                var newField = this.state[actions[i]["newLocation"][0]][actions[i]["newLocation"][1]];
                 for (let j = 0; j < newField.length; j++) {
                     if (newField[j].type == "actor" || newField[j].type == "solid") {
                         actions[i]["conflict"]=true;
@@ -133,49 +143,60 @@ class World {
             }
         }
 
-        // apply changes for all objects in world state
+        // Apply changes for all objects in world state
         for (let i = 0; i < actions.length; i++) {
             if (actions[i]["conflict"] == false) {
-                var newField = this.state[actions[i]["nextLocation"][0]][actions[i]["nextLocation"][1]];
+                // Add actor to new field
+                var newLocation = actions[i]["newLocation"];
+                var newField = this.state[newLocation[0]][newLocation[1]];
                 newField.push(actions[i]["object"]);
-                var oldField = this.state[actions[i]["currentLocation"][0]][actions[i]["currentLocation"][1]];
-                this.state[actions[i]["currentLocation"][0]][actions[i]["currentLocation"][1]] = oldField.filter(e => e!=actions[i]["object"])
-                //oldField.splice(actions[i]["object"],1);
+
+                // Remove actor from old filed
+                var oldLocation = actions[i]["currentLocation"];
+                var oldField = this.state[oldLocation[0]][oldLocation[1]];
+                this.state[oldLocation[0]][oldLocation[1]] = oldField.filter(e => e!=actions[i]["object"])
             }
         }
 
-        // process items
+        // Process items
         //TODO: move thins into the actor calss - pass item to actor method and make it update itself. But removing the item from the world still needs to be done here
         for (let i = 0; i < actions.length; i++) {
             if (actions[i]["conflict"] == false) {
-                var currentField = this.state[actions[i]["nextLocation"][0]][actions[i]["nextLocation"][1]];
+                var location = actions[i]["newLocation"];
+                var currentField = this.state[location[0]][location[1]];
                 for (var j = 0; j < currentField.length; j++) {
                     if (currentField[j].type == "item") {
-                        //remove item
-                        this.state[actions[i]["nextLocation"][0]][actions[i]["nextLocation"][1]] = currentField.filter(e => e!=currentField[j])
-                        //add new item
-                        this.addGrass(this.state);
-                        //update actor stats
-                        actions[i]["object"].life = Math.min(100, actions[i]["object"].life+10);
+                        //Update actor stats
+                        actions[i]["object"].processItem(currentField[j]);
+
+                        //Remove item
+                        this.state[location[0]][location[1]] = currentField.filter(e => e!=currentField[j])
+                        
+                        //Add new item
+                        var x = Math.floor(Math.random() * this.height);
+                        var y = Math.floor(Math.random() * this.width);
+                        while (!this.addSquare(this.state, Grass));
                     }
                 }
             }
         }
 
-        //remove dead actors and add new ones
+        // Remove dead actors and add new ones
         for (let i = 0; i < actions.length; i++) {
             if (actions[i]["conflict"] == false) {
-                var currentField = this.state[actions[i]["nextLocation"][0]][actions[i]["nextLocation"][1]];
+                var location = actions[i]["newLocation"];
+                var currentField = this.state[location[0]][location[1]];
                 if (actions[i]["object"].life <= 0) {
-                    //remove actor
-                    this.state[actions[i]["nextLocation"][0]][actions[i]["nextLocation"][1]] = currentField.filter(e => e!=actions[i]["object"]);
-                    //add new one
-                    this.addActor(this.state);
+                    // Remove actor
+                    this.state[location[0]][location[1]] = currentField.filter(e => e!=actions[i]["object"]);
+                    
+                    // Add new one
+                    while (!this.addSquare(this.state, Robot));
                 }
             }
         }
 
-        // update stats
+        // Update stats
         for (var i = 0; i < actions.length; i++) {
             if (actions[i]["object"].selected) {
                 this.life = actions[i]["object"].life;
@@ -188,14 +209,14 @@ class World {
 
     }
 
+    // Function to draw the world
     draw() {
-        // draw world
         const canvas = document.getElementById("flatland");
         const ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (var i = 0; i < this.height; i++) {
             for (var j = 0; j < this.width; j++) {
-                // determine which object to draw at top
+                // Determine which object to draw at top
                 var max=-1;
                 var objectToDraw;
                 for (var x = 0; x < this.state[i][j].length; x++){
@@ -203,16 +224,17 @@ class World {
                         objectToDraw = this.state[i][j][x];
                     }
                 }
-                // draw object
+
+                // Draw object
                 ctx.beginPath();
-                ctx.rect(i * SQUARESIZE, j * SQUARESIZE, objectToDraw.width, objectToDraw.height);
+                ctx.rect(i * config.squareSize, j * config.squareSize, objectToDraw.width, objectToDraw.height);
                 ctx.fillStyle = objectToDraw.color;
                 ctx.fill();
                 ctx.closePath();
             }
         }
 
-        //draw UI
+        // Draw UI
         var updateLife = document.getElementById("life");
         var updateAge = document.getElementById("age");
         var updateMaxAge = document.getElementById("maxAge");
@@ -222,18 +244,21 @@ class World {
     }
   }
 
+// The robot class that represents an actor with a hardcoded behavior algorithm
 class Robot {
     constructor() {
         this.type = "actor";
-        this.width = SQUARESIZE;
-        this.height = SQUARESIZE;
+        this.width = config.squareSize;
+        this.height = config.squareSize;
         this.z = 2;
         this.color = "red";
         this.life = 100;
         this.age = 0;
         this.selected = true;
     }
+
     getInputs() {};
+
     update() {
         this.life--;
         this.age++;
@@ -248,44 +273,55 @@ class Robot {
           }
         return outputs;
     }
+
+    processItem(Item) {
+        if (Item.constructor.name == "Grass") {
+            this.life = Math.min(100, this.life += 10);
+        }
+    }
 }
 
+// Just a solid rock
 class Rock {
     constructor() {
         this.type="solid";
-        this.width=SQUARESIZE;
-        this.height=SQUARESIZE;
+        this.width=config.squareSize;
+        this.height=config.squareSize;
         this.z=1;
         this.color="gray";
     }
 }
 
+// The grass class represents an item that provides energy
 class Grass {
     constructor() {
         this.type="item";
-        this.width=SQUARESIZE;
-        this.height=SQUARESIZE;
+        this.width=config.squareSize;
+        this.height=config.squareSize;
         this.z=1;
         this.color="green";
     }
 }
 
+// The default terrain class
 class Earth {
     constructor() {
         this.type="terrain";
-        this.width=SQUARESIZE;
-        this.height=SQUARESIZE;
+        this.width=config.squareSize;
+        this.height=config.squareSize;
         this.z=0;
         this.color="brown";
     }
 }
 
+// The logger class takes care of logging metadata stats like framerate or update speed
 class Logger {
     constructor() {
         this.frameTimes = [];
         this.updateTimes= [];
         this.updateCount= [];
     }
+
     logFrameTime(time) {
         this.frameTimes.push(time);
         this.frameTimes = this.frameTimes.slice("-100");
@@ -311,25 +347,25 @@ class Logger {
     };
 }
 
-//main
-const SQUARESIZE = 10;
+// Main
 
-//create world
+// Create world
 var world = new World();
 world.generate();
 world.draw();
-//world.addSquare(new Robot(50,50));
 
-//create logger
+// Initiate logger
 var logger = new Logger();
 
-//GAME LOOP
+// Game loop
 var lastDraw = 0;
-var speed = 1000/100; //updates per 1000 miliseconds
+var speed = config.speed;
 var doUpdate = 0;
 var lastUpdate = 0;
 var lastUpdateCycle = 0;
-//UPDATE LOOP // webworker - the world update function works via a web worker because requestAnimationFeame stops when tab is not in focus.
+
+// Update loop
+// The world update function works via a web worker because requestAnimationFeame stops when tab is not in focus
 var w = new Worker("webworker.js");
 w.onmessage = function(event) {
     doUpdate += (performance.now()-lastUpdateCycle)/speed;
@@ -342,7 +378,9 @@ w.onmessage = function(event) {
     }
     lastUpdateCycle = performance.now();   
   };
-//DRAWING LOOP // the drawing loop works via requestAnimationFrame
+
+//Drawing loop
+// The drawing loop works via requestAnimationFrame
 function drawingLoop(timeStamp) {
     logger.logFrameTime(timeStamp-lastDraw);
     world.draw();
