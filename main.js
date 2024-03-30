30bdde8aa6f828a4f44f0de46ee2ee56b0cd8747
stankoj <stanko.jankovic@live.com>
2024-03-30 23:37:49 +0100

    made change to allow only forward connectios as a test

--- main.js
+++ main.js
@@ -33,9 +33,9 @@ class World {
         this.height = config.worldHeight;
         this.width = config.worldWidth;
         this.bestBrain = false;
-        this.populationSize = 500;
-        this.keepBestBrains = 50;
-        this.keepOtherBrains = 5;
+        this.populationSize = 1000;
+        this.keepOtherBrains = 50;
+        this.keepBestBrains = 10;
         this.population = [];
         this.bestBrains = [];
         this.populationNextGen = [];
